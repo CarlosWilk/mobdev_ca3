6dcef9a4000f123e0ae908e3ba2a9c9a445a1838
Wilker Paulino <carloswilkersp@hotmail.com>
2022-11-13 10:39:31 +0000

    quotes working

--- mobdev-ca3/src/app/pages/quotes/quotes.page.ts
+++ mobdev-ca3/src/app/pages/quotes/quotes.page.ts
@@ -16,14 +16,6 @@ export class QuotesPage implements OnInit {
 
   ngOnInit() {
       this.quotes = this.api.getQuotes();
-      this.quotes.subscribe(data => {
-      console.log('my data: ', data);
-    });
-  }
-
-  openDetails(quote){
-      let quoteId = quote.quote_id;
-      this.router.navigateByUrl(`/tabs/quotes/${quoteId}`);
   }
 
 }
